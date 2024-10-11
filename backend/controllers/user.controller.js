e928dff9304a33de07faf63fe8540cd8588abb8f
sakibhasansufi <sakibhasansufi@gmail.com>
2024-10-11 19:27:44 +0600

    cloudinary connected

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import { User } from "../models/user.model.js";
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import getDataUri from "../utils/datauri.js";
+import cloudinary from "../utils/cloudinary.js";
 
 
 
@@ -136,6 +138,12 @@ export const updateProfile = async (req,res) =>{
    
     try {
         const {fullName,email,currentPassword,newPassword,phoneNumber,bio,skills} = req.body;
+
+        const file = req.file;
+        const fileUri =getDataUri(file);
+        const cloudResponse = await cloudinary.uploader.upload(fileUri.content)
+
+
         const userId = req.id;
         let skillsArray;
         if(skills){
@@ -186,7 +194,10 @@ export const updateProfile = async (req,res) =>{
         if(bio) user.profile.bio = bio
         if(skills) user.profile.skills = skillsArray
         
-        
+        if(cloudResponse){
+            user.profile.resume = cloudResponse.secure_url
+            user.profile.resumeOriginalName = file.originalname
+        }
 
         await user.save();
 
