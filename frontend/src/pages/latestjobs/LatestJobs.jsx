b0bffcf8f440a083370f70a8730a396cf6442652
sakibhasansufi <sakibhasansufi@gmail.com>
2024-11-29 11:26:31 +0600

    modified the skeleton

--- frontend/src/pages/latestjobs/LatestJobs.jsx
+++ frontend/src/pages/latestjobs/LatestJobs.jsx
@@ -1,16 +1,32 @@
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 import LatestJobCard from "./LatestJobCard";
 import Skeleton from "../home/Skeleton";
 
 const LatestJobs = () => {
-    const { allJobs,loading } = useSelector((state) => state.job);
+    const { allJobs, loading } = useSelector((state) => state.job);
+    const [showSkeleton, setShowSkeleton] = useState(true);
+
+    useEffect(() => {
+        if (!loading) {
+            const timeout = setTimeout(() => {
+                setShowSkeleton(false);
+            }, 3000);
+            return () => clearTimeout(timeout); 
+        } else {
+            setShowSkeleton(true); 
+        }
+    }, [loading]);
+
     return (
         <section className="my-10">
-            <div className="max-w-7xl mx-auto ">
-                <h1 className="text-2xl md:text-4xl text-center font-semibold"><span className="text-[#FF6500]">Latest & Top Job</span> Openings</h1>
+            <div className="max-w-7xl mx-auto">
+                <h1 className="text-2xl md:text-4xl text-center font-semibold">
+                    <span className="text-[#FF6500]">Latest & Top Job</span> Openings
+                </h1>
                 <section className="flex justify-center md:flex-none">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-10">
-                        {loading ? (
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-10">
+                        {showSkeleton ? (
                             <Skeleton count={12} />
                         ) : allJobs.length <= 0 ? (
                             <div className="flex justify-center">
@@ -21,7 +37,6 @@ const LatestJobs = () => {
                         )}
                     </div>
                 </section>
-
             </div>
         </section>
     );
